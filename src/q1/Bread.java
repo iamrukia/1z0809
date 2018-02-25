e42c940536f8cfccd8717ebe321f853165f88589
Nan Sha <iamshanan@gmail.com>
2018-02-25 21:47:18 +0000

    q1 done

--- src/q1/Bread.java
+++ src/q1/Bread.java
@@ -2,7 +2,7 @@ package q1;
 
 public class Bread extends Cake {
 
-    public void bakes(int minutes, int emperature){}
+    //public void bake(int minutes, int emperature){}
     public void addToppings(){}
 
     public static void main(String[] args) {
